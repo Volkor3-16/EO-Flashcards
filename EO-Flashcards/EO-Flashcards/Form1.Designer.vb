9837ba1c5d9fef9cc126169de840f0cfb7c19743
AConsolePeasant <AConsolePeasant@openmailbox.org>
2016-07-25 16:24:05 +1000

    Main Login Page
    Added login to design, started on Main Menu.

--- EO-Flashcards/EO-Flashcards/Form1.Designer.vb
+++ EO-Flashcards/EO-Flashcards/Form1.Designer.vb
@@ -22,17 +22,102 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.btnAdminLogin = New System.Windows.Forms.Button()
+        Me.txtAdmin = New System.Windows.Forms.TextBox()
+        Me.lblAdmin = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.txtUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.txtUsername.Location = New System.Drawing.Point(11, 40)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(191, 29)
+        Me.txtUsername.TabIndex = 1
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnLogin.Location = New System.Drawing.Point(12, 75)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(190, 35)
+        Me.btnLogin.TabIndex = 2
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'lblUsername
+        '
+        Me.lblUsername.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!)
+        Me.lblUsername.Location = New System.Drawing.Point(13, 13)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(189, 24)
+        Me.lblUsername.TabIndex = 3
+        Me.lblUsername.Text = "Enter your Username"
+        '
+        'btnAdminLogin
+        '
+        Me.btnAdminLogin.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdminLogin.Location = New System.Drawing.Point(237, 75)
+        Me.btnAdminLogin.Name = "btnAdminLogin"
+        Me.btnAdminLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdminLogin.TabIndex = 4
+        Me.btnAdminLogin.Text = "Login"
+        Me.btnAdminLogin.UseVisualStyleBackColor = True
+        '
+        'txtAdmin
+        '
+        Me.txtAdmin.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtAdmin.Location = New System.Drawing.Point(237, 49)
+        Me.txtAdmin.Name = "txtAdmin"
+        Me.txtAdmin.Size = New System.Drawing.Size(75, 20)
+        Me.txtAdmin.TabIndex = 5
+        '
+        'lblAdmin
+        '
+        Me.lblAdmin.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblAdmin.AutoSize = True
+        Me.lblAdmin.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!)
+        Me.lblAdmin.Location = New System.Drawing.Point(234, 24)
+        Me.lblAdmin.Name = "lblAdmin"
+        Me.lblAdmin.Size = New System.Drawing.Size(85, 13)
+        Me.lblAdmin.TabIndex = 6
+        Me.lblAdmin.Text = "Admin Password"
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 270)
+        Me.ClientSize = New System.Drawing.Size(324, 134)
+        Me.Controls.Add(Me.lblAdmin)
+        Me.Controls.Add(Me.txtAdmin)
+        Me.Controls.Add(Me.btnAdminLogin)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.txtUsername)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "frmMain"
         Me.Text = "Esperanto Flash Cards"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents btnAdminLogin As Button
+    Friend WithEvents txtAdmin As TextBox
+    Friend WithEvents lblAdmin As Label
 End Class
--- EO-Flashcards/EO-Flashcards/Form1.vb
+++ EO-Flashcards/EO-Flashcards/Form1.vb
@@ -1,5 +1,3 @@
 ﻿Public Class frmMain
-    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
 End Class
