be6abdbe9cf366f518e297524a9eb96929b785cd
AConsolePeasant <AConsolePeasant@openmailbox.org>
2016-07-25 17:31:12 +1000

    Added Opening forms
    Because I'm not good enough to add passwords and stuff yet, just
    clicking login should send you to the next forms.

--- EO-Flashcards/EO-Flashcards/Form1.vb
+++ EO-Flashcards/EO-Flashcards/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class frmMain
-
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        frmMenu.Show()
+        Me.Close()
+    End Sub
 End Class
--- EO-Flashcards/EO-Flashcards/frmMenu.vb
+++ EO-Flashcards/EO-Flashcards/frmMenu.vb
@@ -1,3 +1,6 @@
 ﻿Public Class frmMenu
-
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        frmAdmin.Show()
+        Me.Close()
+    End Sub
 End Class
