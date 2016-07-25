9a9495a8db5ec3bd174e02f207b06ab08a4616f7
AConsolePeasant <AConsolePeasant@openmailbox.org>
2016-07-25 18:10:02 +1000

    Added notes
    Added comments to the code.

--- EO-Flashcards/EO-Flashcards/frmMenu.vb
+++ EO-Flashcards/EO-Flashcards/frmMenu.vb
@@ -1,6 +1,19 @@
-﻿Public Class frmMenu
+﻿' Esperanto Flash Cards
+' Author - AConsolePeasant
+' Last modified - Check on the git page, I shouldn't have to update this for you.
+'
+'----------------------------------------------------------------------------
+' "THE BEER-WARE LICENSE" (Revision 42):
+' <AConsolePeasant@openmailbox.org> wrote this file. As long as you retain this notice you
+' can do whatever you want with this stuff. If we meet some day, And you think
+' this stuff Is worth it, you can buy me a beer in return.
+' ----------------------------------------------------------------------------
+'
+
+Public Class frmMenu
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
         frmAdmin.Show()
         Me.Close()
+        ' Opens Admin Menu and closes frmMenu when clicked.
     End Sub
 End Class
