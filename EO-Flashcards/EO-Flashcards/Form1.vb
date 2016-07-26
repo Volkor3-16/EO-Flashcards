b3d08a3414822d0d29fbd66706dcef2a7d2a247a
AConsolePeasant <AConsolePeasant@users.noreply.github.com>
2016-07-26 12:23:35 +1000

    Update Form1.vb
    Removed License

--- EO-Flashcards/EO-Flashcards/Form1.vb
+++ EO-Flashcards/EO-Flashcards/Form1.vb
@@ -1,14 +1,6 @@
 ﻿' Esperanto Flash Cards
 ' Author - AConsolePeasant
 ' Last modified - Check on the git page, I shouldn't have to update this for you.
-'
-'----------------------------------------------------------------------------
-' "THE BEER-WARE LICENSE" (Revision 42):
-' <AConsolePeasant@openmailbox.org> wrote this file. As long as you retain this notice you
-' can do whatever you want with this stuff. If we meet some day, And you think
-' this stuff Is worth it, you can buy me a beer in return.
-' ----------------------------------------------------------------------------
-'
 
 Public Class frmMain
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
