c14b07e0837cfa967bfecc21ba2ecea745fdc79c
AConsolePeasant <AConsolePeasant@openmailbox.org>
2016-08-17 18:00:13 +1000

    Breaking stuff
    Okay, I'm trying to get ther sorting stuff all working, but I can't
    really use the code I stole from stackoverflow.

---
+++ EO-Flashcards/EO-Flashcards/arrModule.vb
@@ -0,0 +1,4 @@
+﻿Module arrModule
+    'This module reads the esperanto dictonary textfile and then puts them into two arrays, one for eo, and one for en.
+    ' " : " as a seperator, so each of the words are working. 
+End Module
---
+++ EO-Flashcards/EO-Flashcards/frmArcade.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmArcade
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(842, 149)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 198)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(843, 179)
+        Me.TextBox2.TabIndex = 1
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(434, 169)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(421, 23)
+        Me.ProgressBar1.TabIndex = 2
+        Me.ProgressBar1.Value = 50
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(13, 169)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.ProgressBar2.RightToLeftLayout = True
+        Me.ProgressBar2.Size = New System.Drawing.Size(424, 23)
+        Me.ProgressBar2.TabIndex = 3
+        Me.ProgressBar2.Value = 50
+        '
+        'frmArcade
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(867, 389)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "frmArcade"
+        Me.Text = "frmArcade"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+End Class
